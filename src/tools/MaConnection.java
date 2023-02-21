76a955bfcc4ae31393781835276b0698a383d868
chaimaghribi21 <ghribi.chaima@esprit.tn>
2023-02-21 16:33:55 +0100

    1st commit

---
+++ src/tools/MaConnection.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tools;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author user
+ */
+public class MaConnection {
+    private Connection cnx;
+        String url = "jdbc:mysql://localhost:3306/taktak-4";
+        String user = "root";
+        String pwd = "";
+        public static MaConnection ct;
+
+    private MaConnection() {
+        try {
+            cnx = DriverManager.getConnection(url,user,pwd);
+            System.out.println("Cnx etablie ");
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+    public static MaConnection getInstance(){
+        if(ct ==null)
+            ct= new MaConnection();
+        return ct;
+    }
+
+    public Connection getCnx() {
+        return cnx;
+    }
+
+   
+    
+}
